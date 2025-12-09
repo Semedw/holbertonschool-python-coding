f1c226fcd2cee3e647350a4f13d8ed0ba6df05d1
Semedw <smusazade2000@gmail.com>
2025-12-09 11:27:54 +0400

    task 0 adding dot to docs

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-This module defines a square by its size
+This module defines a square by its size.
 """
 
 
 class Square:
     """
-    This is the inside of the square class
+    This is the inside of the square class.
     """ 
     def __init__(self, size):
         """
-        This function is initialized when an object is created
+        This function is initialized when an object is created.
         """
         self.__size = size        
