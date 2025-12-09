1d97b90370970e3ebfb366e4e8b3a4e91e56298c
Semedw <smusazade2000@gmail.com>
2025-12-09 17:42:50 +0400

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This script create an object and it checks whether the size is suitible or not
+"""
+
+
+class Square:
+    """
+    This class create a square object
+    """
+    def __init__(self, size=0):
+        """
+        This method run when an object is initialized
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+        """
+        return self.__size**2
+
+    @property
+    def size(self):
+        """
+        Property for the size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter of the size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+    
+    def my_print(self):
+        """
+        Printing the area of the square
+        """
+        if size>0:
+            print(f"#{self.__size**2}")
+        else:
+            print()
