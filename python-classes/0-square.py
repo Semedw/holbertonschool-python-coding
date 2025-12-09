e4cd5cbc971ec80ca3338fb5ef87d36a4545c495
Semedw <smusazade2000@gmail.com>
2025-12-09 11:29:46 +0400

    task 0 pep8 final isle artiq

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,7 +7,7 @@ This module defines a square by its size.
 class Square:
     """
     This is the inside of the square class.
-    """ 
+    """
     def __init__(self, size):
         """
         This function is initialized when an object is created.
