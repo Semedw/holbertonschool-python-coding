e4ca82a110596dae2676fe2f4c115372c30a7193
Semedw <smusazade2000@gmail.com>
2025-12-09 11:19:34 +0400

    task0

---
+++ python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a square by its size
+"""
+
+
+class Square:
+    """
+    This is the inside of the square class
+    """
+    
+    def __init__(self, size):
+        """
+        This function is initialized when an object is created
+        """
+        self__size = size
+
